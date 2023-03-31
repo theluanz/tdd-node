914e252bc7e5fd31cfcad68bd4018214c268b10b
theluanz <luanzuffo.lz@gmail.com>
2023-03-30 22:06:01 -0300

    Remove console.log

--- controllers/UpdateTodo/UpdateTodo.js
+++ controllers/UpdateTodo/UpdateTodo.js
@@ -5,8 +5,6 @@ async function updateTodo({ id, name, date, isCompleted }) {
     throw new Error('Todo não existe');
   });
   if (!todoExists) throw new Error('Todo não existe');
-  console.log('todoExists', todoExists);
-  console.log(name, date, isCompleted);
 
   const updatedTodo = await prismaClient.todo.update({
     where: { id },
